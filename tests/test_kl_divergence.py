647a1924fa46f6599ffae81a9a848191b093c7c9
Sergiy Matusevych <sergiym@microsoft.com>
2021-10-11 17:19:03 -0700

    add unit test for KL div loss function

--- tests/test_kl_divergence.py
+++ tests/test_kl_divergence.py
@@ -11,3 +11,9 @@ def test_kl_divergence_func():
     q = np.array([1./3, 1./3, 1./3])
     assert dml.dml._kl_divergence(p, q) == pytest.approx(0.0852996)
     assert dml.dml._kl_divergence(q, p) == pytest.approx(0.097455)
+
+
+def test_kl_divergence_loss():
+    gather = [np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3])]
+    assert dml.kl_divergence_loss(gather, 0) == pytest.approx(0.0852996)
+    assert dml.kl_divergence_loss(gather, 1) == pytest.approx(0.097455)
