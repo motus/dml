minor fix to kl div test - make sure it matches the example from wikipedia

tests/test_kl_divergence.py
@@ -7,7 +7,7 @@ import dml
 
 
 def test_kl_divergence_func():
-    p = np.array([9, 12, 4], dtype=np.float) / 25.0
-    q = np.ones(3, dtype=np.float) / 3.0
+    p = np.array([9./25, 12./25, 4./25])
+    q = np.array([1./3, 1./3, 1./3])
     assert dml.dml._kl_divergence(p, q) == pytest.approx(0.0852996)
     assert dml.dml._kl_divergence(q, p) == pytest.approx(0.097455)