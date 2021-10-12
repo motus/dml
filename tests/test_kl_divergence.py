19419005ae561886b3a4be55a781ced39e2e2ee3
Sergiy Matusevych <sergiym@microsoft.com>
2021-10-11 17:22:28 -0700

    add unit test for 4-way KL divergence loss

--- tests/test_kl_divergence.py
+++ tests/test_kl_divergence.py
@@ -13,7 +13,16 @@ def test_kl_divergence_func():
     assert dml.dml._kl_divergence(q, p) == pytest.approx(0.097455)
 
 
-def test_kl_divergence_loss():
+def test_kl_divergence_loss2():
     gather = [np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3])]
     assert dml.kl_divergence_loss(gather, 0) == pytest.approx(0.0852996)
     assert dml.kl_divergence_loss(gather, 1) == pytest.approx(0.097455)
+
+
+def test_kl_divergence_loss4():
+    gather = [np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3]),
+              np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3])]
+    assert dml.kl_divergence_loss(gather, 0) == pytest.approx(0.0568664)
+    assert dml.kl_divergence_loss(gather, 1) == pytest.approx(0.06497)
+    assert dml.kl_divergence_loss(gather, 2) == pytest.approx(0.0568664)
+    assert dml.kl_divergence_loss(gather, 3) == pytest.approx(0.06497)
