9ca4b2e425a6396fd6c77edd800a36bde3bb2e5e
Sergiy Matusevych <sergiym@microsoft.com>
2021-10-12 15:00:55 -0700

    implement dot product loss function (suboptimal, uses np.diagonal())

--- dml/loss.py
+++ dml/loss.py
@@ -16,7 +16,7 @@ def null(gather, i):
 
 
 def _kl_divergence_impl(p, q):
-    "KL Divergence of two vectors"
+    "KL Divergence of two vectors or matrices"
     return np.sum(p * np.log(p / q), axis=len(p.shape) - 1)
 
 
@@ -33,4 +33,26 @@ def kl_divergence(gather, i):
     """
     p = gather[i]
     return sum(_kl_divergence_impl(p, q) for (j, q) in enumerate(gather)
+               if j != i) / (len(gather) - 1)
+
+
+def _dot_product_impl(p, q):
+    "Row-wise dot product of two vectors or matrices"
+    res = p.dot(q.T)
+    return res.diagonal() if isinstance(res, np.ndarray) else res
+
+
+def dot_product(gather, i):
+    """
+    Dot Product loss. An average of row-wise dot products
+    of `i` and all other vectors or matrices in `gather`.
+
+    gather: a list of NN output from all nodes.
+    i: index of the current node.
+
+    returns the average value of row-wise dot products
+    of data `i` against all other values in the `gather` list.
+    """
+    p = gather[i]
+    return sum(_dot_product_impl(p, q) for (j, q) in enumerate(gather)
                if j != i) / (len(gather) - 1)
---
+++ tests/loss/test_dot_product.py
@@ -0,0 +1,47 @@
+# pylint: disable=missing-docstring
+
+import pytest
+import numpy as np
+
+import dml.loss
+
+
+def test_dot_product_vec2():
+    gather = [np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3])]
+    assert dml.loss.dot_product(gather, 0) == pytest.approx(1./3)
+    assert dml.loss.dot_product(gather, 1) == pytest.approx(1./3)
+
+
+def test_dot_product_vec4():
+    gather = [
+        np.array([9./25, 12./25, 4./25]), np.array([9./25, 12./25, 4./25]),
+        np.array([1./3, 1./3, 1./3]), np.array([1./3, 1./3, 1./3])
+    ]
+    assert dml.loss.dot_product(gather, 0) == pytest.approx(0.3507556)
+    assert dml.loss.dot_product(gather, 1) == pytest.approx(0.3507556)
+    assert dml.loss.dot_product(gather, 2) == pytest.approx(1./3)
+    assert dml.loss.dot_product(gather, 3) == pytest.approx(1./3)
+
+
+def test_dot_product_impl_matrix():
+    p = np.array([[9./25, 12./25, 4./25], [9./25, 12./25, 4./25],
+                  [1./3, 1./3, 1./3], [1./3, 1./3, 1./3]])
+    q = np.array([[9./25, 12./25, 4./25], [1./3, 1./3, 1./3],
+                  [9./25, 12./25, 4./25], [1./3, 1./3, 1./3]])
+    assert dml.loss._dot_product_impl(p, q) == pytest.approx(
+        np.array([0.3856, 1./3, 1./3, 1./3]))
+    assert dml.loss._dot_product_impl(q, p) == pytest.approx(
+        np.array([0.3856, 1./3, 1./3, 1./3]))
+
+
+def test_dot_product_loss_matrix():
+    gather = [
+        np.array([[9./25, 12./25, 4./25], [9./25, 12./25, 4./25],
+                 [1./3, 1./3, 1./3], [1./3, 1./3, 1./3]]),
+        np.array([[9./25, 12./25, 4./25], [1./3, 1./3, 1./3],
+                 [9./25, 12./25, 4./25], [1./3, 1./3, 1./3]])
+    ]
+    assert dml.loss.dot_product(gather, 0) == pytest.approx(
+        np.array([0.3856, 1./3, 1./3, 1./3]))
+    assert dml.loss.dot_product(gather, 1) == pytest.approx(
+        np.array([0.3856, 1./3, 1./3, 1./3]))
