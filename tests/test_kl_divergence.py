eaa56dfefdfea3129290b34f4c7488079eb78dba
Sergiy Matusevych <sergiym@microsoft.com>
2021-10-11 17:24:21 -0700

    reorder some data in unit tests

--- tests/test_kl_divergence.py
+++ tests/test_kl_divergence.py
@@ -20,9 +20,9 @@ def test_kl_divergence_loss2():
 
 
 def test_kl_divergence_loss4():
-    gather = [np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3]),
-              np.array([9./25, 12./25, 4./25]), np.array([1./3, 1./3, 1./3])]
+    gather = [np.array([9./25, 12./25, 4./25]), np.array([9./25, 12./25, 4./25]),
+              np.array([1./3, 1./3, 1./3]), np.array([1./3, 1./3, 1./3])]
     assert dml.kl_divergence_loss(gather, 0) == pytest.approx(0.0568664)
-    assert dml.kl_divergence_loss(gather, 1) == pytest.approx(0.06497)
-    assert dml.kl_divergence_loss(gather, 2) == pytest.approx(0.0568664)
+    assert dml.kl_divergence_loss(gather, 1) == pytest.approx(0.0568664)
+    assert dml.kl_divergence_loss(gather, 2) == pytest.approx(0.06497)
     assert dml.kl_divergence_loss(gather, 3) == pytest.approx(0.06497)
